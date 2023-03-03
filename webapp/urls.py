564ffecc8693b3212316daa19aea7e716253be82
yedige87 <yedabdygapparov@gmail.com>
2023-03-03 14:14:01 +0600

    изменена функция добавления на класс, изменен add.html

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from webapp.views.todos import add_view, ToDoEditView, ToDoView, delete_view
+from webapp.views.todos import ToDoAddView, ToDoEditView, ToDoView, delete_view
 from webapp.views.base import index_view
 
 urlpatterns = [
     path("", index_view, name='index'),
-    path('add', add_view, name='todo_add'),
+    path('add', ToDoAddView.as_view(), name='todo_add'),
     path('edit/<int:pk>', ToDoEditView.as_view(), name='todo_edit'),
     path('todo/<int:pk>', ToDoView.as_view(), name='todo_view'),
     path('delete/<int:pk>', delete_view, name='todo_delete'),
